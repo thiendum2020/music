3e9a44e27255778a1f9574f22645c2cb970510b8
thiendum2020 <83193243+thiendum2020@users.noreply.github.com>
2022-05-03 13:30:22 +0700

    create user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,37 @@
+const { User, validate } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+// Get all users        GET/api/users
+exports.getAllUsers = async (req, res) => {
+    const users = await User.find().select("-password -__v");
+    res.status(200).send({
+        data: users,
+        message: "Get all users successfully",
+    });
+};
+
+// Get user by id       GET/api/users/:id
+exports.getUserByID = async (req, res) => {
+    const user = await User.findById(req.params.id).select("-password -__v");
+    res.status(200).send({
+        data: user,
+        message: "Get User By ID successfully",
+    });
+};
+
+// Update user by id        PUT/api/users/:id
+exports.updateUserByID = async (req, res) => {
+    const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }).select(
+        "-password -__v",
+    );
+    res.status(200).send({
+        data: user,
+        message: "Profile updated successfully",
+    });
+};
+
+// Delete user by id        DELETE/api/users/:id
+exports.deleteUserByID = async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send({ message: "User deleted successfully" });
+};
