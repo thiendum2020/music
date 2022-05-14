f0bf1c9f278805d81cfecbf766f98e5f018598b6
thiendum2020 <83193243+thiendum2020@users.noreply.github.com>
2022-05-14 20:04:06 +0700

    create api upload file and image

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary");
+
 const connection = require("./db");
 
 const authRoutes = require("./routes/authRoutes");
@@ -10,7 +15,15 @@ const songRoutes = require("./routes/songRoutes");
 const artistRoutes = require("./routes/artistRoutes");
 const playListRoutes = require("./routes/playlistRoutes");
 const searchRoutes = require("./routes/searchRoutes");
+const upload = require("./routes/upload");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    fileUpload({
+        useTempFiles: true,
+    }),
+);
 
 connection();
 app.use(cors());
@@ -22,6 +35,15 @@ app.use("/api/songs", songRoutes);
 app.use("/api/artists", artistRoutes);
 app.use("/api/playlists/", playListRoutes);
 app.use("/api/search", searchRoutes);
+app.use("/api/", upload);
+
+// Setting up cloudinary configuration
+cloudinary.config({
+    cloud_name: "toryobuyall",
+    api_key: "543991772329632",
+    api_secret: "5W2B1kJ7r6NIq1p6-WB7vIGj0cs",
+    secure: true,
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
