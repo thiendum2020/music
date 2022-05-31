b9423028a1ac9b3a4ad0e3ae69137014d5509a5e
thiendum2020 <83193243+thiendum2020@users.noreply.github.com>
2022-05-31 14:16:25 +0700

    redux-saga

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,49 +1,49 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import {publishedSongsReducer} from './reducers/songReducers.js'
-import logger from 'redux-logger'
-import {authReducer} from './reducers/authReducers.js'
-
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { publishedSongsReducer } from "./reducers/songReducers.js";
+import logger from "redux-logger";
+import { authReducer } from "./reducers/authReducers.js";
+import createSagaMiddleware from "redux-saga";
 
 const rootReducer = combineReducers({
     publishedSongsReducer,
-    authReducer
+    authReducer,
 });
 
-// lấy user login tử localstorage 
-const userLoginFromStorage = localStorage.getItem('userLogin') ? JSON.parse(localStorage.getItem('userLogin')) : null
+// lấy user login tử localstorage
+const userLoginFromStorage = localStorage.getItem("userLogin") ? JSON.parse(localStorage.getItem("userLogin")) : null;
 
 const preloadedState = {
     authReducer: {
         isAuthenticated: userLoginFromStorage ? true : false,
-        userLogin: userLoginFromStorage ? userLoginFromStorage.user : null
-    }
-}
+        userLogin: userLoginFromStorage ? userLoginFromStorage.user : null,
+    },
+};
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: rootReducer,
     preloadedState,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
-})
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware().concat(logger), sagaMiddleware],
+});
 
-export default store
+export default store;
 
 // import { createStore, combineReducers, applyMiddleware } from 'redux'
 // import thunk from 'redux-thunk'
 // import { composeWithDevTools } from 'redux-devtools-extension'
 
-
 // const reducer = combineReducers({
-    
+
 // })
 
-// //lấy user login tử localstorage 
+// //lấy user login tử localstorage
 // const userLoginFromStorage = localStorage.getItem('userLogin') ? JSON.parse(localStorage.getItem('userLogin')) : null
 
 // //lấy giỏ hàng từ localstorage về tương ứng vs id của user login
 // const cartItemsFromStorage = userLoginFromStorage && localStorage.getItem(userLoginFromStorage.user._id) ? JSON.parse(localStorage.getItem(userLoginFromStorage.user._id)) : []
 
 // const shippingInfoFromStorage = localStorage.getItem('shippingInfo') ? JSON.parse(localStorage.getItem('shippingInfo')) : {}
-
 
 // //khởi tạo giá trị, kiểm tra đã có user đăng nhập hay chưa
 // const initialState = {
